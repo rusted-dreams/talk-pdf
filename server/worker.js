00e9bb454913c722d983dc1e95aa1c9a0698317b
rusted-dreams <123829783+rusted-dreams@users.noreply.github.com>
2025-06-29 02:05:04 +0530

    worker almost done

--- server/worker.js
+++ server/worker.js
@@ -1,16 +1,52 @@
 import { Worker } from "bullmq";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { QdrantVectorStore } from "@langchain/qdrant";
+import { Document } from "@langchain/core/documents";
+import { PDFLoader } from '@langchain/community/document_loaders/fs/pdf';
+import { CharacterTextSplitter } from '@langchain/textsplitters';
+import dotenv from "dotenv"
 
+dotenv.config({});
+
+const OPENAI_API = process.env.OPENAI_API
 
 const worker = new Worker(
   'file-upload-queue',
   async (job) => {
-    console.log('job', job.data);
+    console.log(`Job:`, job.data);
+    const data = JSON.parse(job.data);
+    /*
+    Path: data.path
+    read the pdf from path,
+    chunk the pdf,
+    call the openai embedding model for every chunk,
+    store the chunk in qdrant db
+    */
+
+    // Load the PDF
+    const loader = new PDFLoader(data.path);
+    const docs = await loader.load();
+
+    const embeddings = new OpenAIEmbeddings({
+      model: 'text-embedding-3-small',
+      apiKey: OPENAI_API,
+    });
+
+    const vectorStore = await QdrantVectorStore.fromExistingCollection(
+      embeddings,
+      {
+        url: 'http://localhost:6333',
+        collectionName: 'langchainjs-testing',
+      }
+    );
+    await vectorStore.addDocuments(docs);
+    console.log(`All docs are added to vector store`);
   },
   {
     concurrency: 100,
     connection: {
-      host: "localhost",
-      port: "6379"
-    }
+      host: 'localhost',
+      port: '6379',
+    },
   }
-)
+);
